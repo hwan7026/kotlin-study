13eaab855541bff9bf928debe99793c61f3d1e63
hwan7026 <hwan@fastviewkorea.com>
2021-08-23 10:23:40 +0900

    회원 정보 명세서 작성

--- src/main/kotlin/com/example/kotlinstudy/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/kotlinstudy/member/controller/MemberController.kt
@@ -19,17 +19,19 @@ class MemberController(
     val memberService: MemberService
 ){
 
-    @Operation(summary = "", description ="")
+    @Operation(summary = "회원 정보 조회")
     @GetMapping("/{id}")
     fun getMember(@PathVariable id: Long) : CommonResponse<ResMemberDTO> {
         return memberService.findById(id)
     }
 
+    @Operation(summary = "회원 정보 저장")
     @PostMapping
     fun saveMember(@RequestBody @Valid memberSaveDTO: ReqMemberSaveDTO) : CommonResponse<ResMemberDTO> {
         return memberService.saveMember(memberSaveDTO)
     }
 
+    @Operation(summary = "회원 정보 수정")
     @PatchMapping("/{id}")
     fun updateMember(
         @Parameter(description = "member ID")@PathVariable id: Long,
@@ -38,6 +40,7 @@ class MemberController(
         return memberService.updateMember(memberUpdateDTO.setId(id))
     }
 
+    @Operation(summary = "회원 정보 삭제")
     @DeleteMapping("/{id}")
     fun deleteMember(@PathVariable id: Long) : ResponseEntity<Any> {
         return ResponseEntity.ok(memberService.deleteById(id))
